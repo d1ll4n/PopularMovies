6bd1e1242b3cf09c8ca54fd899906b661a31fde3
Dillan de Langen <dillan@sfactorial.ai>
2018-01-04 21:03:18 +0200

    feat: Add MovieDb utility class.
    Added a class containing the constants used to retrieve data from the the MovieDb API.

---
+++ app/src/main/java/ai/sfactorial/popularmovies/utilities/MovieDb.java
@@ -0,0 +1,18 @@
+package ai.sfactorial.popularmovies.utilities;
+
+/**
+ * Created by dillan on 2018/01/03.
+ */
+
+public class MovieDb {
+    public static final String Size_W92 = "w92";
+    public static final String Size_W154 = "w154";
+    public static final String Size_W185 = "w185";
+    public static final String Size_W342 = "w342";
+    public static final String Size_W500 = "w500";
+    public static final String Size_W780 = "w780";
+    public static final String Size_Original = "original";
+
+    public static final String Order_Popular = "movie/popular";
+    public static final String Order_Top_Rated = "movie/top_rated";
+}
