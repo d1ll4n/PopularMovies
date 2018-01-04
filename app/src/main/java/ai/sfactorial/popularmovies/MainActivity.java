5adf6062822e1bb6cb7cb8f3ef266108d2f202df
Dillan de Langen <dillan@sfactorial.ai>
2018-01-04 21:15:55 +0200

    feat: start AsyncTask when MainActivity is created.

--- app/src/main/java/ai/sfactorial/popularmovies/MainActivity.java
+++ app/src/main/java/ai/sfactorial/popularmovies/MainActivity.java
@@ -1,17 +1,67 @@
 package ai.sfactorial.popularmovies;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.GridView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import ai.sfactorial.popularmovies.adapters.ImageAdapter;
+import ai.sfactorial.popularmovies.models.Settings;
+import ai.sfactorial.popularmovies.utilities.MovieDb;
+import ai.sfactorial.popularmovies.utilities.Network;
+
+public class MainActivity extends AppCompatActivity{
+    MovieAdapter mAdapter;
+    ListView mMovieList;
+    GridView mMoviesGrid;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //mMovieList = (ListView) findViewById(R.id.lv_movies);
+        mMoviesGrid = (GridView) findViewById(R.id.gv_movies);
+
+        try{
+            URL url = Network.buildQueryUrl(Settings.SortOrder);
+            MovieDbQueryTask movieDbQueryTask = new MovieDbQueryTask(this);
+            movieDbQueryTask.execute(url);
+        }
+        catch (Exception e){
+            //Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
+            e.printStackTrace();
+        }
+    }
+
+    public void onSearchClick(View view){
+
+
+    }
+
+    public void onListItemClick(AdapterView<?> adapterView, View view, int position, long blah){
+        Toast.makeText(this, "Item " + String.valueOf(position) + " clicked.", Toast.LENGTH_LONG).show();
     }
 
     @Override
